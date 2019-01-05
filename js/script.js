f2df3ee5f3b43f14de001ec91c4362a42e4c2696
The-Steve <stevenpcrawford@gmail.com>
2019-01-04 20:34:46 -0500

    Changed comments
    wrote my own comments as per the instructions given

---
+++ js/script.js
@@ -0,0 +1,75 @@
+/******************************************
+Treehouse Techdegree:
+FSJS project 1 - A Random Quote Generator
+******************************************/
+
+/*
+  Create array of quote objects
+*/
+
+var quotes = [
+  {
+   quote:"When you reach the end of your rope, tie a knot in it and hang on.",
+   author:"Franklin D. Roosevelt",
+   year: 1792,
+   citation: "A speech of some sort"
+  },
+  {
+   quote:"You cannot shake hands with a clenched fist.",
+   author:"Indira Gandhi",
+   year: 20,
+   citation: "A sheepskin scroll"
+  },
+  {
+   quote:"Learning never exhausts the mind.",
+   author:"Leonardo da Vinci",
+   year: 1374,
+   citation: "A brain fragment"
+  },
+  {
+   quote:"No act of kindness, no matter how small, is ever wasted.",
+   author:"Aesop",
+   year: 100,
+   citation: "Unknown"
+  },
+  {
+   quote:"There is only one corner of the universe you can be certain of improving, and that's your own self.",
+   author:"Aldous Huxley",
+   year: 1903,
+   citation: "Your Corner (not boxing) a memorandum of sheer wit"
+  },
+  {
+   quote:"The journey of a thousand miles begins with one step.",
+   author:"Lao Tzu",
+   year: 1,
+   citation: "Spoken to the wind and its been floating ever since"
+  },
+];
+
+
+/*
+  getRandomQuote, generate a random number within array bounds and return the selected object
+*/
+function getRandomQuote(){
+  var rand = Math.floor(Math.random() * quotes.length);
+  return quotes[rand];
+}
+
+
+
+/*
+  printQuote, takes the quote object and converts it to HTML
+*/
+function printQuote () {
+  var theQuote = getRandomQuote();
+  var str = "<p class ='quote'>" + theQuote.quote + "</p>"
+  document.getElementById("quote-box").innerHTML = str;
+}
+
+
+
+/*
+  Show another quote button. click to generate another random quote
+*/
+
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);

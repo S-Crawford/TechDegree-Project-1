6f38c78a0d199752a3e9c6a2ebf819e867d7b0da
The-Steve <stevenpcrawford@gmail.com>
2019-01-10 12:15:23 -0500

    changed quotes to reflect modular abilities of gRQ

--- js/script.js
+++ js/script.js
@@ -11,37 +11,34 @@ var quotes = [
   {
    quote:"When you reach the end of your rope, tie a knot in it and hang on.",
    author:"Franklin D. Roosevelt",
-   year: 1792,
+   year: 1936,
    citation: "A speech of some sort"
   },
   {
    quote:"You cannot shake hands with a clenched fist.",
    author:"Indira Gandhi",
-   year: 20,
-   citation: "A sheepskin scroll"
+   year: 1966,
   },
   {
    quote:"Learning never exhausts the mind.",
    author:"Leonardo da Vinci",
-   year: 1374,
-   citation: "A brain fragment"
+   year: 1498,
   },
   {
    quote:"No act of kindness, no matter how small, is ever wasted.",
    author:"Aesop",
-   year: 100,
-   citation: "Unknown"
+   year: "582 BC",
   },
   {
    quote:"There is only one corner of the universe you can be certain of improving, and that's your own self.",
    author:"Aldous Huxley",
-   year: 1903,
+   year: 1953,
    citation: "Your Corner (not boxing) a memorandum of sheer wit"
   },
   {
    quote:"The journey of a thousand miles begins with one step.",
    author:"Lao Tzu",
-   year: 1,
+   //year: "540 BC",
    citation: "Spoken to the wind and its been floating ever since"
   },
 ];
